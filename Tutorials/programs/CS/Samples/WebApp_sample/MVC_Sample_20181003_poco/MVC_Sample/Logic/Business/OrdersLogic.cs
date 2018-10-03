40c4f60e794946a839b5c3682c9f1d55e17c592f
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2018-10-03 18:34:19 +0900

    Added workbench of MVC for customize to fit poco style.

---
+++ Tutorials/programs/CS/Samples/WebApp_sample/MVC_Sample_20181003_poco/MVC_Sample/Logic/Business/OrdersLogic.cs
@@ -0,0 +1,68 @@
+﻿// 業務フレームワーク
+using Touryo.Infrastructure.Business.Business;
+using Touryo.Infrastructure.Business.Common;
+using Touryo.Infrastructure.Business.Dao;
+using Touryo.Infrastructure.Business.Exceptions;
+using Touryo.Infrastructure.Business.Presentation;
+using Touryo.Infrastructure.Business.Util;
+
+// フレームワーク
+using Touryo.Infrastructure.Framework.Business;
+using Touryo.Infrastructure.Framework.Common;
+using Touryo.Infrastructure.Framework.Dao;
+using Touryo.Infrastructure.Framework.Exceptions;
+using Touryo.Infrastructure.Framework.Presentation;
+using Touryo.Infrastructure.Framework.Util;
+using Touryo.Infrastructure.Framework.Transmission;
+
+// 部品
+using Touryo.Infrastructure.Public.Db;
+using Touryo.Infrastructure.Public.IO;
+using Touryo.Infrastructure.Public.Log;
+using Touryo.Infrastructure.Public.Str;
+using Touryo.Infrastructure.Public.Util;
+
+using MVC_Sample.Logic.Common;
+using MVC_Sample.Logic.Dao;
+
+namespace MVC_Sample.Logic.Business
+{
+    public class OrdersLogic : MyFcBaseLogic
+    {
+        private void UOC_GetOrders(OrderParameterValue orderParameter)
+        {
+            // DAO 集約クラスを生成する
+            ConsolidatedLayerD facade = new ConsolidatedLayerD(this.GetDam());
+
+            // 注文情報一覧を取得する
+            OrderReturnValue returnValue = facade.GetOrders(orderParameter);
+
+            // 戻り値クラスを返す
+            this.ReturnValue = returnValue;
+        }
+
+        private void UOC_GetOrderById(OrderParameterValue orderParameter)
+        {
+            // DAO 集約クラスを生成する
+            ConsolidatedLayerD facade = new ConsolidatedLayerD(this.GetDam());
+
+            // 注文情報の詳細を取得する
+            OrderReturnValue returnValue = facade.GetOrderById(orderParameter);
+
+            // 戻り値クラスを返す
+            this.ReturnValue = returnValue;
+        }
+
+        private void UOC_UpdateOrder(OrderParameterValue orderParameter)
+        {
+            // DAO 集約クラスを生成する
+            ConsolidatedLayerD facade = new ConsolidatedLayerD(this.GetDam());
+
+            // 注文情報をDBに登録する
+            OrderReturnValue returnValue = facade.UpdateOrder(orderParameter);
+
+            // 戻り値クラスを返す
+            this.ReturnValue = returnValue;
+        }
+    }
+}
